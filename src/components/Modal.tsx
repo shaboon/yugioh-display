166efed1b7db5d013028090a371fd7a34d61ce9d
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-05-14 21:31:12 -0400

    Creating Modal
    Modal prepared

---
+++ src/components/Modal.tsx
@@ -0,0 +1,7 @@
+export default function Modal () {
+    return (
+        <div>
+            <h1>Modal Page</h1>
+        </div>
+    )
+}
---
+++ src/components/pages/Board.tsx
@@ -0,0 +1,7 @@
+export default function Board () {
+    return (
+        <div>
+            <h1>Board Page</h1>
+        </div>
+    )
+}
