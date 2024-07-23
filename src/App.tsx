7bf2463f05739e7c09e5c2c938bdb7a2d4f144a0
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-07-23 09:23:25 -0400

    Styling added for Application
    Application Styling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Landing from './components/pages/Landing';
@@ -23,12 +22,18 @@ function App() {
     for (let i = 0; i < 20; i++) {
       // console.log(cardData[i].name);
       const container = document.getElementById('api-test');
+      const cardContainer = document.createElement("div");
+
+      // This the cards pulled from the API
       const cardIMG = document.createElement("img");
+      cardIMG.className = 'card-img drop-shadow-md m-2';
       cardIMG.src = cardData[randomStart + i].card_images[0].image_url;
       cardIMG.width = 100;
       cardIMG.alt = cardData[randomStart + i].name;
 
-      container?.appendChild(cardIMG);
+
+      container?.appendChild(cardContainer);
+      cardContainer?.appendChild(cardIMG);
   
     }
     
@@ -38,11 +43,12 @@ function App() {
   handleAPICall();
 
   return (
-    <div className="App">
+    <div className="App flex bg-gradient-to-t from-purple-500 to-indigo-900">
         <Landing />
         <Search />
-        <div className='api-test flex' id='api-test'>
+        <div className='api-test flex flex-wrap bg-gray-600 justify-center rounded-md drop-shadow-xl' id='api-test'>
           <h1>API Test</h1>
+          <p>API Test</p>
         </div>
     </div>
   );
