86a8f5594ef59a1b4e26bf007e888dbaeb85f548
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2024-07-22 20:06:04 -0400

    Found a Way
    Found a way to pull the info and use it
    
    Admittedly it was pretty simple, but I also was trying to avoid doing it in the fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,28 @@ import Search from './components/Search';
 // https://db.ygoprodeck.com/api/v7/cardinfo.php
 
 function App() {
-  let cardData = "";
 
   const handleAPICall = async () => {
     const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php');
     const data = await response.json();
 
-    cardData = data.data;
-    console.log(cardData[0]); 
+    const cardData = data.data;
+    console.log(cardData[0]);
+    console.log(cardData.length);
 
+    const randomStart = Math.floor(Math.random() * cardData.length);
+    for (let i = 0; i < 20; i++) {
+      // console.log(cardData[i].name);
+      const container = document.getElementById('api-test');
+      const cardIMG = document.createElement("img");
+      cardIMG.src = cardData[randomStart + i].card_images[0].image_url;
+      cardIMG.width = 100;
+      cardIMG.alt = cardData[randomStart + i].name;
+
+      container?.appendChild(cardIMG);
+  
+    }
+    
     console.log(data);
   }
 
@@ -28,7 +41,7 @@ function App() {
     <div className="App">
         <Landing />
         <Search />
-        <div className='api-test'>
+        <div className='api-test flex' id='api-test'>
           <h1>API Test</h1>
         </div>
     </div>
